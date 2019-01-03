2ec19b76b47b928ac54d3dc6fd4425c93a2ef5d4
christian <chgad.games@gmail.com>
2019-01-03 20:59:50 +0100

    added Contract migration

--- api/models.py
+++ api/models.py
@@ -15,13 +15,18 @@ class User(models.Model):
 class Contract(models.Model):
 
     id = models.UUIDField(primary_key=True)
-    user = models.ForeignKey(to=User, related_name="contracts", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        to=User, related_name="contracts", on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=100)
     hours = models.FloatField()
     start_date = models.DateField()
     end_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(to=User, related_name="+", on_delete=models.CASCADE) # No backwards relation to these Fields
+    created_by = models.ForeignKey(
+        to=User, related_name="+", on_delete=models.CASCADE
+    )  # No backwards relation to these Fields
     modified_at = models.DateTimeField(auto_now=True)
-    modified_by = models.ForeignKey(to=User, related_name="+", on_delete=models.CASCADE) # No backwards relation to these Fields
-
+    modified_by = models.ForeignKey(
+        to=User, related_name="+", on_delete=models.CASCADE
+    )  # No backwards relation to these Fields
