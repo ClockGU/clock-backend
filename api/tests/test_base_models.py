0a8d0c0034f30b562fc9d2a31401757ffe4a31d8
christian <chgad.games@gmail.com>
2019-01-03 20:26:20 +0100

    added basic test for Contract model

--- api/tests/test_base_models.py
+++ api/tests/test_base_models.py
@@ -21,6 +21,25 @@ class TestUserModelExists:
         assert issubclass(User, models.Model)
 
 
+class TestContractModelExists:
+    def test_model_existence(self):
+        """
+        This Test tests if an Object User can be imported.
+        :return:
+        """
+
+        from api.models import Contract
+
+    def test_model_is_model(self):
+        """
+        Test if the User Object is a Django Model
+        :return:
+        """
+        from api.models import Contract
+
+        assert issubclass(Contract, models.Model)
+
+
 class TestUserFields:
     """
     This Testsuit summerizes the basic field tests:
@@ -85,3 +104,71 @@ class TestUserFields:
             user_model_class_instace._meta.get_field("modified_at"),
             models.DateTimeField,
         )
+
+
+class TestContractFields:
+    """
+    This Testsuit summerizes the basic field tests:
+    1. Do all fields exist
+    2. Do all fields have the correct format/class instance
+    """
+
+    def test_model_has_id(self, contract_model_class_instance):
+        assert hasattr(contract_model_class_instance, "id")
+
+    def test_model_has_user(self, contract_model_class_instance):
+        assert hasattr(contract_model_class_instance, "user")
+
+    def test_model_has_name(self, contract_model_class_instance):
+        assert hasattr(contract_model_class_instance, "name")
+
+    def test_model_has_hours(self, contract_model_class_instance):
+        assert hasattr(contract_model_class_instance, "hours")
+
+    def test_model_has_start_date(self, contract_model_class_instance):
+        assert hasattr(contract_model_class_instance, "start_date")
+
+    def test_model_has_end_date(self, contract_model_class_instance):
+        assert hasattr(contract_model_class_instance, "end_date")
+
+    def test_model_has_created_at(self, contract_model_class_instance):
+        assert hasattr(contract_model_class_instance, "created_at")
+
+    def test_model_has_created_by(self, contract_model_class_instance):
+        assert hasattr(contract_model_class_instance, "created_by")
+
+    def test_model_has_modified_at(self, contract_model_class_instance):
+        assert hasattr(contract_model_class_instance, "modified_at")
+
+    def test_model_has_modified_by(self, contract_model_class_instance):
+        assert hasattr(contract_model_class_instance, "modified_by")
+
+    def test_field_type_id(self, contract_model_class_instance):
+        assert isinstance(contract_model_class_instance._meta.get_field("id"), models.UUIDField)
+
+    def test_field_type_user(self,contract_model_class_instance):
+        assert isinstance(contract_model_class_instance._meta.get_field("user"), models.ForeignKey)
+
+    def test_field_type_name(self,contract_model_class_instance):
+        assert isinstance(contract_model_class_instance._meta.get_field("name"), models.CharField)
+        
+    def test_field_type_hours(self,contract_model_class_instance):
+        assert isinstance(contract_model_class_instance._meta.get_field("hours"), models.FloatField)
+        
+    def test_field_type_start_date(self,contract_model_class_instance):
+        assert isinstance(contract_model_class_instance._meta.get_field("start_date"), models.DateField)
+        
+    def test_field_type_end_date(self, contract_model_class_instance):
+        assert isinstance(contract_model_class_instance._meta.get_field("end_date"), models.DateField)
+
+    def test_field_type_created_at(self, contract_model_class_instance):
+        assert isinstance(contract_model_class_instance._meta.get_field("created_at"), models.DateTimeField)
+        
+    def test_field_type_created_by(self, contract_model_class_instance):
+        assert isinstance(contract_model_class_instance._meta.get_field("created_by"), models.ForeignKey)
+
+    def test_field_type_modified_at(self, contract_model_class_instance):
+        assert isinstance(contract_model_class_instance._meta.get_field("modified_at"), models.DateTimeField)
+
+    def test_field_type_modified_by(self, contract_model_class_instance):
+        assert isinstance(contract_model_class_instance._meta.get_field("modified_by"), models.ForeignKey)
