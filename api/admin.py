13332bd62d3900bd1f8665648b26964aa14c7a26
christian <chgad.games@gmail.com>
2019-01-06 16:24:16 +0100

    added Report model to admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
-from api.models import User, Contract, Shift
+from api.models import User, Contract, Shift, Report
 
 
 class UserAdmin(admin.ModelAdmin):
@@ -57,3 +57,23 @@ class ShiftAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Shift, ShiftAdmin)
+
+
+class ReportAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "link_user", "month_year", "created_at", "modified_at")
+
+    def link_user(self, obj):
+        """
+                Creates a link to the corresponding User object to display in the columns.
+                :param obj:
+                :return: string
+                """
+        user = obj.user
+        url = reverse("admin:api_user_change", args=[user.pk])
+        return format_html('<a href="{}">{}</a>', url, user.pk)
+
+    link_user.short_description = "user"
+
+
+admin.site.register(Report, ReportAdmin)
