4b961e4ed9ddf7e75b7f93b2752b7219422c9a2b
christian <chgad.games@gmail.com>
2019-02-02 12:58:18 +0100

    implement ContractSerializer with custom Validation and Tests

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from api.models import User, Contract, Shift, Report
+from pytz import datetime
 
 
 @pytest.fixture
@@ -20,3 +21,68 @@ def shift_model_class():
 @pytest.fixture
 def report_model_class():
     return Report
+
+
+@pytest.fixture
+def user_object():
+    return User.objects.create_user(
+        email="test@test.com",
+        first_name="Testfirstname",
+        last_name="Testlastname",
+        personal_number="1234567890",
+        password="Test_password",
+    )
+
+
+@pytest.fixture
+def valid_contract_json(user_object):
+    name = "Test Contract"
+    hours = 20.0
+    start_date = datetime.date(2019, 1, 1)
+    end_date = datetime.date(2019, 1, 31)
+    user = user_object.id
+
+    created_at = datetime.datetime(2018, 12, 31, hour=10)
+    modified_at = created_at
+
+    data = {
+        "name": name,
+        "hours": hours,
+        "start_date": start_date,
+        "end_date": end_date,
+        "user": user,
+        "created_by": user,
+        "modified_by": user,
+        "created_at": created_at,
+        "modified_at": modified_at,
+    }
+
+    return data
+
+
+@pytest.fixture
+def end_date_before_start_date_contract_json(valid_contract_json):
+    start_date = datetime.date(2019, 2, 1)
+    valid_contract_json["start_date"] = start_date
+    return valid_contract_json
+
+
+@pytest.fixture
+def start_date_day_incorrect_contract_json(valid_contract_json):
+    start_date = datetime.date(2019, 1, 6)
+    valid_contract_json["start_date"] = start_date
+    return valid_contract_json
+
+
+@pytest.fixture
+def end_date_day_incorrect_contract_json(valid_contract_json):
+    end_date = datetime.date(2019, 1, 22)
+    valid_contract_json["end_date"] = end_date
+    return valid_contract_json
+
+
+@pytest.fixture
+def negative_hours_contract_json(valid_contract_json):
+    hours = -20.0
+    valid_contract_json["hours"] = hours
+    return valid_contract_json
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls import url
+from rest_framework.routers import DefaultRouter
 
 from api.views import index
 
+
+router = DefaultRouter()
+
+
 urlpatterns = [
     # Demonstration url for celery
-    path("celery-dummy", index, name="index")
+    path("celery-dummy", index, name="index"),
+    path("", include(router.urls)),
 ]
