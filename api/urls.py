a7e08260b97799c29fdde52776b642f42f41a22b
christian <chgad.games@gmail.com>
2019-02-23 18:25:08 +0100

    First commit for ShiftViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api.views import index, ContractViewSet
+from api.views import index, ContractViewSet, ShiftViewSet
 
 app_name = "api"
 router = DefaultRouter()
 router.register(r"contracts", ContractViewSet, base_name="contracts")
+router.register(r"shifts", ShiftViewSet, base_name="shifts")
 
 urlpatterns = [
     # Demonstration url for celery
