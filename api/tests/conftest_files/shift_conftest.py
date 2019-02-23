1b0284c54e076563dcd5c23565b1c91dbdb1da1e
christian <chgad.games@gmail.com>
2019-02-23 16:23:35 +0100

    added valid json for a shift object, a TagSerializerField and mor configuration to ShiftSerializer

---
+++ api/tests/conftest_files/shift_conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from pytz import datetime
+
+
+@pytest.fixture
+def valid_shift_json(user_object, contract_object):
+    started = datetime.datetime(2019, 1, 29, 14)
+    stopped = datetime.date(2019, 1, 29, 16)
+    created_at = datetime.datetime(2019, 1, 29, 16).isoformat()
+    modified_at = created_at
+    user = user_object.id
+    contract = contract_object.id
+    type = "st"
+    note = "something was strange"
+    tags = ["tag1, tag2"]
+
+    data = {
+        "started": started,
+        "stopped": stopped,
+        "contract": contract,
+        "type": type,
+        "note": note,
+        "tags": tags,
+        "user": user,
+        "created_by": user,
+        "modified_by": user,
+        "created_at": created_at,
+        "modified_at": modified_at,
+    }
