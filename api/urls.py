ac6653ed25ddadc618eca0561f5138eab4bfb700
christian <chgad.games@gmail.com>
2019-03-16 17:20:08 +0100

    implemented Reports API Endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api.views import index, ContractViewSet, ShiftViewSet
+from api.views import index, ContractViewSet, ShiftViewSet, ReportViewSet
 
 app_name = "api"
 router = DefaultRouter()
 router.register(r"contracts", ContractViewSet, base_name="contracts")
 router.register(r"shifts", ShiftViewSet, base_name="shifts")
+router.register(r"reports", ReportViewSet, base_name="reports")
 
 list_month_year_shifts = ShiftViewSet.as_view({"get": "list_month_year"})
 
