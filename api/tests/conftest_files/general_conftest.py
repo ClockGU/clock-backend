579b66e924c7cbf90021b5377695d50239aed975
christian <chgad.games@gmail.com>
2019-04-02 14:23:09 +0200

    Prepare Docstrings for conftest files

--- api/tests/conftest_files/general_conftest.py
+++ api/tests/conftest_files/general_conftest.py
@@ -1,7 +1,15 @@
 import pytest
 
 from rest_framework.test import APIClient
+from rest_framework.test import APIRequestFactory, force_authenticate
+from rest_framework.request import Request, QueryDict
+from django.urls import reverse
+
 from api.models import User, Contract, Shift, Report
+
+
+# This conftest file provides all necessary test data concerning project classes and auxiliary functions/classes.
+# It will be imported by the conftest.py in the parent directory.
 
 
 @pytest.fixture
@@ -27,3 +35,15 @@ def report_model_class():
 @pytest.fixture
 def client():
     return APIClient()
+
+
+@pytest.fixture
+def plain_request_object(user_object):
+    """
+    This fixture provides a plain request object.
+    :param user_object:
+    :return:
+    """
+    request = APIRequestFactory().get(reverse("user-me"), data=QueryDict())
+    force_authenticate(request, user=user_object)
+    return Request(request)
