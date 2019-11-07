93af75ad3fe5419e961c7d6df5a266d154d5f5ec
christian <chgad.games@gmail.com>
2019-11-07 20:00:56 +0100

    Black format migration.

--- api/migrations/0006_creating_clockedinshift_model.py
+++ api/migrations/0006_creating_clockedinshift_model.py
@@ -8,22 +8,57 @@ import uuid
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('api', '0005_support_uuid_pk_tags'),
-    ]
+    dependencies = [("api", "0005_support_uuid_pk_tags")]
 
     operations = [
         migrations.CreateModel(
-            name='ClockedInShift',
+            name="ClockedInShift",
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False, unique=True)),
-                ('started', models.DateTimeField()),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('modified_at', models.DateTimeField(auto_now=True)),
-                ('contract', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='clocked_in_shift', to='api.Contract')),
-                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='+', to=settings.AUTH_USER_MODEL)),
-                ('modified_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='+', to=settings.AUTH_USER_MODEL)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='clocked_in_shift', to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.UUIDField(
+                        default=uuid.uuid4,
+                        editable=False,
+                        primary_key=True,
+                        serialize=False,
+                        unique=True,
+                    ),
+                ),
+                ("started", models.DateTimeField()),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("modified_at", models.DateTimeField(auto_now=True)),
+                (
+                    "contract",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="clocked_in_shift",
+                        to="api.Contract",
+                    ),
+                ),
+                (
+                    "created_by",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+                (
+                    "modified_by",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="+",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+                (
+                    "user",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="clocked_in_shift",
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
-        ),
+        )
     ]
