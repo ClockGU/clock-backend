f4c412167744073c7d02ae4d9ab4c89ee1d2acd8
christian <chgad.games@gmail.com>
2019-11-07 20:00:37 +0100

    Add ClockedInShiftViewset urls.

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,19 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import ContractViewSet, ReportViewSet, ShiftViewSet, index
+from api.views import (
+    ContractViewSet,
+    ReportViewSet,
+    ShiftViewSet,
+    ClockedInShiftViewSet,
+    index,
+)
 
 app_name = "api"
 router = DefaultRouter()
 router.register(r"contracts", ContractViewSet, base_name="contracts")
 router.register(r"shifts", ShiftViewSet, base_name="shifts")
+router.register(r"clockedinshifts", ClockedInShiftViewSet, base_name="clockedinshifts")
 router.register(r"reports", ReportViewSet, base_name="reports")
 
 list_month_year_shifts = ShiftViewSet.as_view({"get": "list_month_year"})
