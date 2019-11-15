50a87928e9a7eb574730ff493ff91c4a6b8c36eb
christian <chgad.games@gmail.com>
2019-11-15 18:59:41 +0100

    Adapt new Time format in Stundenzettel Export for tests.

--- api/tests/test_utilities.py
+++ api/tests/test_utilities.py
@@ -13,12 +13,12 @@ from api.utilities import relativedelta_to_string
 
 def test_relativedelta_to_string_positive_delta(positive_relativedelta_object):
     result_string = relativedelta_to_string(positive_relativedelta_object)
-    assert result_string == "148:23:15"
+    assert result_string == "148:23"
 
 
 def test_relativedelta_to_string_negative_delta(negative_relativedelta_object):
     result_string = relativedelta_to_string(negative_relativedelta_object)
-    assert result_string == "-148:23:15"
+    assert result_string == "-148:23"
 
 
 @freeze_time("2020-04-10")
