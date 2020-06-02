47d56446e90bf97db093036f93c5bfedfa937aa1
Michael Gecht <gecht.m@gmail.com>
2020-06-02 19:30:39 +0200

    refactor: remove GoetheUniAccount

--- api/oauth/providers/goetheuni/provider.py
+++ api/oauth/providers/goetheuni/provider.py
@@ -1,32 +1,11 @@
-from allauth.socialaccount import app_settings
 from allauth.socialaccount.providers.base import ProviderAccount
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
-
-
-class GoetheUniAccount(ProviderAccount):
-    def to_str(self):
-        dflt = super(GoetheUniAccount, self).to_str()
-        return next(
-            value
-            for value in (
-                self.account.extra_data.get("name", None),
-                self.account.extra_data.get("login", None),
-                dflt,
-            )
-            if value is not None
-        )
 
 
 class GoetheUniProvider(OAuth2Provider):
     id = "goetheuni"
     name = "GoetheUni"
-    account_class = GoetheUniAccount
-
-    def get_default_scope(self):
-        scope = []
-        if app_settings.QUERY_EMAIL:
-            scope.append("user:email")
-        return scope
+    account_class = ProviderAccount
 
     def extract_uid(self, data):
         return str(data["id"])
