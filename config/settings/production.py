ffb2c283e58dd32103faff5623db2618cefe7a34
Michael Gecht <gecht.m@gmail.com>
2020-07-02 18:50:52 +0200

    project: revert settings changes, make flake8 ignore files

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,7 +1,8 @@
+# flake8: noqa
 import logging
 import sys
 
-from .common import INSTALLED_APPS, MIDDLEWARE, env
+from .common import *
 
 # SECRET CONFIGURATION
 SECRET_KEY = env("DJANGO_SECRET_KEY")
