0b499f96a7246e3e98c4475d8279f12219c13680
christian <chgad.games@gmail.com>
2020-12-27 15:50:54 +0100

    Add URL parameter filtering to shift endpoint.

--- api/tests/test_filters.py
+++ api/tests/test_filters.py
@@ -1,15 +1,32 @@
+import django_filters
+
 from api.filters import ShiftFilterSet
 
 
 class TestShiftFilterSet:
-    def test_was_reviewed_filter(self):
-        assert "was_reviewed" in ShiftFilterSet._meta.fields
+    def test_contract_filter(self):
+        assert "contract" in ShiftFilterSet.get_filters()
 
-    def test_was_reviewed_lookups(self):
-        assert ShiftFilterSet._meta.fields["was_reviewed"] == ["exact"]
+    def test_year_filter(self):
+        assert "year" in ShiftFilterSet.get_filters()
 
-    def test_started_filter(self):
-        assert "started" in ShiftFilterSet._meta.fields
+    def test_month_filter(self):
+        assert "month" in ShiftFilterSet.get_filters()
 
-    def test_started_lookups(self):
-        assert ShiftFilterSet._meta.fields["started"] == ["exact", "lte"]
+    def test_contract_lookups(self):
+        filter = ShiftFilterSet.get_filters().get("contract")
+        assert isinstance(filter, django_filters.UUIDFilter)
+        assert filter.field_name == "contract"
+        assert filter.lookup_expr == "exact"
+
+    def test_year_lookups(self):
+        filter = ShiftFilterSet.get_filters().get("year")
+        assert isinstance(filter, django_filters.NumberFilter)
+        assert filter.field_name == "started"
+        assert filter.lookup_expr == "year"
+
+    def test_month_lookups(self):
+        filter = ShiftFilterSet.get_filters().get("month")
+        assert isinstance(filter, django_filters.NumberFilter)
+        assert filter.field_name == "started"
+        assert filter.lookup_expr == "month"
