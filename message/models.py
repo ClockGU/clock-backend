a1c039489ac666fd25307f2dbfe8610402855d35
christian <chgad.games@gmail.com>
2020-12-28 19:34:46 +0100

    Create new 'message' app analogue to solid apps.

---
+++ message/models.py
@@ -0,0 +1,20 @@
+from datetime import date
+
+from django.db import models
+
+
+class Message(models.Model):
+    """
+    Model for a message that is displayed to the users when the app starts.
+    """
+
+    MTYPE_CHOICES = [("CL", "Changelog"), ("SE", "Series"), ("NO", "Notice")]
+
+    type = models.CharField(max_length=2, choices=MTYPE_CHOICES)
+    title = models.CharField(max_length=100)
+    text = models.TextField(default="", blank=True, verbose_name="text (Markdown)")
+    valid_from = models.DateField(default=date.today)
+    valid_to = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
