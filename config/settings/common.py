075ed87019d113a9dafe546f2c838929d2281f54
christian <chgad.games@gmail.com>
2021-08-15 12:52:38 +0200

    Isort.

--- config/settings/common.py
+++ config/settings/common.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 from datetime import timedelta
 
 import environ
-from django.utils.translation import gettext_lazy as _
 from corsheaders.defaults import default_headers
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
