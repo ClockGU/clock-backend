7a4c8172c84aabc1c3331c067518d87da639aefc
Sören Grieger <soeren.grieger@posteo.de>
2022-11-01 14:01:14 +0100

    reordered validation in shifts serializer, added new vailadtion for total worktime per day, reordered the test for the shifts serializer and created although new tests

--- api/tests/test_serializers.py
+++ api/tests/test_serializers.py
@@ -537,6 +537,42 @@ class TestShiftSerializerValidation:
                 context={"request": plain_request_object},
             ).is_valid(raise_exception=True)
 
+    @freeze_time("2019-01-01 00:00:00+00:00")
+    @pytest.mark.django_db
+    def test_shift_in_future_was_reviewed_fails(
+        self, shift_starting_in_future_was_reviewed_querydict, plain_request_object
+    ):
+        with pytest.raises(serializers.ValidationError):
+            ShiftSerializer(
+                data=shift_starting_in_future_was_reviewed_querydict,
+                context={"request": plain_request_object},
+            ).is_valid(raise_exception=True)
+
+    def test_reviewed_shift_on_sunday_not_allowed(self, shift_on_a_sunday_json_querydict, plain_request_object):
+        with pytest.raises(serializers.ValidationError):
+            ShiftSerializer(
+                data=shift_on_a_sunday_json_querydict,
+                context={"request": plain_request_object},
+            ).is_valid(raise_exception=True)
+
+    def test_reviewed_shift_on_holiday_typ_holliday_allowed(self):
+        pass
+
+    def test_reviewed_shift_on_holiday_typ_normal_not_allowed(self):
+        pass
+
+    def test_reviewed_vacation_shift_day_exclusive(self):
+        pass
+
+    def test_reviewed_sick_shift_day_exclusive(self):
+        pass
+
+    def test_reducing_day_worktime_by_needed_breaktime(self):
+        pass
+
+    def test_creating_shift_in_not_owned_contract_fails(self):
+        pass
+
     @pytest.mark.django_db
     def test_reviewed_shift_on_holiday_typ_normal_not_allowed(
         self, normal_shift_on_a_holiday_json_querydict, plain_request_object
