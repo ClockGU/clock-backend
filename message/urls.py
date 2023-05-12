9c699acafdb838b7ac37dadc2b30a3b8d3ae56f9
Sören Grieger <soeren.grieger@posteo.de>
2023-05-12 12:54:47 +0200

    fixing message url

--- message/urls.py
+++ message/urls.py
@@ -4,6 +4,6 @@ from .views import MessageEndpoint
 
 app_name = "message"
 router = SimpleRouter()
-router.register(r"messages", MessageEndpoint)
+router.register(r"message", MessageEndpoint, basename="messages")
 urlpatterns = []
 urlpatterns += router.urls
