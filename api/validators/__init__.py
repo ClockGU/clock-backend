abeaf787a11cceb929e72a1eff25e38de1ad3831
Christian Grossmüller <chgad.games@gmail.com>
2024-01-19 13:29:38 +0100

    Add regular Employee and civil servant as worktime model.

--- api/validators/__init__.py
+++ api/validators/__init__.py
@@ -4,6 +4,8 @@ from .student_employee import StudentEmployeeValidator
 
 VALIDATOR_CLASS_NAMES = (
     ("studEmp", "Studentische Hilfskraft"),
+    ("regEmp", "Regulär*er Angestelt*er"),
+    ("civilSer", "Beamt*in")
 )
 
 VALIDATOR_CLASSES = {
