650efbdbece96328cca63bde9a045d50c7b162f7
Christian Grossmüller <chgad.games@gmail.com>
2024-02-01 14:16:27 +0100

    Change notes composition.

--- api/views.py
+++ api/views.py
@@ -13,7 +13,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://github.com/ClockGU/clock-backend/blob/master/licenses/>.
 """
-
+import requests
 import weasyprint
 from dateutil.relativedelta import relativedelta
 from django.conf import settings
@@ -335,7 +335,7 @@ class ReportViewSet(viewsets.ReadOnlyModelViewSet):
 
             # 1: Arbeitszeit > 10 h
             if worktime > datetime.timedelta(hours=10):
-                notes_list.append(1)
+                notes_list.append("1")
 
             # 2: Pausenzeit zu gering
             if (
@@ -345,7 +345,7 @@ class ReportViewSet(viewsets.ReadOnlyModelViewSet):
                 worktime > datetime.timedelta(hours=9)
                 and breaktime < datetime.timedelta(minutes=45)
             ):
-                notes_list.append(2)
+                notes_list.append("2")
 
             # 3: Ruhezeiten < 11 h
             if shifts_of_yesterday.exists():
@@ -354,7 +354,7 @@ class ReportViewSet(viewsets.ReadOnlyModelViewSet):
                 if (
                     shifts_of_date.first().started - shifts_of_yesterday.last().stopped
                 ) < datetime.timedelta(hours=11):
-                    notes_list.append(3)
+                    notes_list.append("3")
 
             # 4: Arbeit nach 20 Uhr
             today_at_20 = datetime.datetime(
@@ -364,7 +364,7 @@ class ReportViewSet(viewsets.ReadOnlyModelViewSet):
                 shifts_of_date.filter(started__gte=today_at_20).exists()
                 or shifts_of_date.filter(stopped__gte=today_at_20).exists()
             ):
-                notes_list.append(4)
+                notes_list.append("4")
 
             # 5: Arbeit vor 8 Uhr
             today_at_8 = datetime.datetime(
@@ -374,11 +374,11 @@ class ReportViewSet(viewsets.ReadOnlyModelViewSet):
                 shifts_of_date.filter(started__lte=today_at_8).exists()
                 or shifts_of_date.filter(stopped__lte=today_at_8).exists()
             ):
-                notes_list.append(5)
+                notes_list.append("5")
 
             # 6: Sonntagsarbeit
             if shifts_of_date[0].started.weekday() == 6:
-                notes_list.append(6)
+                notes_list.append("6")
 
             # 7: Feiertagsarbeit
             de_he_holidays = country_holidays("DE", subdiv="HE")
@@ -386,9 +386,9 @@ class ReportViewSet(viewsets.ReadOnlyModelViewSet):
                 shifts_of_date[0].started.strftime("%Y-%m-%d") in de_he_holidays
                 and shifts_of_date.first().type != "bh"
             ):
-                notes_list.append(7)
+                notes_list.append("7")
 
-            notes = str(notes_list).replace("[", "").replace("]", "")
+            notes = ", ".join(notes_list)
 
             started = shifts_of_date.first().started.astimezone(
                 timezone(settings.TIME_ZONE)
