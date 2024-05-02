09914d5f06741c960b1b16d534d92b4ef83198ed
Christian Grossmüller <chgad.games@gmail.com>
2024-05-02 18:08:03 +0200

    Add email sending functionality to .save method on creation.

---
+++ supervisor_api/encryption.py
@@ -0,0 +1,20 @@
+import base64
+import json
+
+from cryptography.fernet import Fernet
+from django.conf import settings
+
+SECRET_BYTES = bytes(settings.SECRET_KEY, "utf-8")
+
+fernet = Fernet(base64.urlsafe_b64encode(SECRET_BYTES[:32]))
+
+
+def encrypt_data(data):
+    if not isinstance(data, str):
+        raise TypeError("Data provided for encryption is not of type String.")
+
+    return fernet.encrypt(bytes(data, "utf-8"))
+
+
+def decrypt_token(token):
+    return json.loads(fernet.decrypt(token))
