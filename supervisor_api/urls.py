fd6ec1358cef4d9b7978b4c8560d4b3625f03034
Christian Grossmüller <chgad.games@gmail.com>
2024-06-07 16:08:08 +0200

    Isort and Black.

--- supervisor_api/urls.py
+++ supervisor_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from supervisor_api.views import VerifyEndpoint, ReportingEndpoint
-
+from supervisor_api.views import ReportingEndpoint, VerifyEndpoint
 
 urlpatterns = [
     path("verify/", VerifyEndpoint.as_view(), name="verify"),
-    path("reports/<int:month>/<int:year>/", ReportingEndpoint.as_view(), name="reports")
+    path(
+        "reports/<int:month>/<int:year>/", ReportingEndpoint.as_view(), name="reports"
+    ),
 ]
