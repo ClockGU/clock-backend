566aa9e8224076cce5fa21cc7ae359bb97ef0f62
Christian Grossmüller <chgad.games@gmail.com>
2024-11-25 15:00:59 +0100

    Too strict evaluation of work after 20:00 oclock.

--- api/views.py
+++ api/views.py
@@ -387,8 +387,8 @@ class ReportViewSet(viewsets.ReadOnlyModelViewSet):
                 year=date.year, month=date.month, day=date.day, hour=20
             )
             if (
-                shifts_of_date.filter(started__gte=today_at_20).exists()
-                or shifts_of_date.filter(stopped__gte=today_at_20).exists()
+                shifts_of_date.filter(started__gt=today_at_20).exists()
+                or shifts_of_date.filter(stopped__gt=today_at_20).exists()
             ):
                 notes_list.append("4")
 
