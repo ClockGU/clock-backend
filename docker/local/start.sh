7a6e070a005d567c42aaf6f1d6e05fb486931ae7
Christian Grossmüller <chgad.games@gmail.com>
2026-05-18 11:36:40 +0200

    Make uvicorn reload.

--- docker/local/start.sh
+++ docker/local/start.sh
@@ -7,4 +7,4 @@ python manage.py compilemessages
 python manage.py collectstatic --noinput
 
 echo "Starting Uvicorn ASGI server!"
-uvicorn config.asgi:application --host 0.0.0.0 --port 8000
+uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --reload
